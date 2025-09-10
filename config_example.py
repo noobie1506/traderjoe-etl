ed31afe597587a07317cdc56558d518f8ffc801c
jack <jackhu@berkeley.edu>
2025-09-10 16:48:59 -0700

    changed config_example.py

---
+++ config_example.py
@@ -0,0 +1,17 @@
+user_addresses = [
+    "0x...",  # user_address1
+    "0x...",  # user_address2
+    "0x..."   # user_address3
+]
+
+pool_addresses = [
+    "0x...",  # pool_address1
+    "0x...",  # pool_address2
+    "0x..."   # pool_address3
+]
+
+tj_dex_key = "YOUR_API_KEY_HERE"
+
+# Start should be before most recent deposit time
+start_time_str = "2024-10-27 00:00:00"
+end_time_str = "2024-11-02 00:00:00"
